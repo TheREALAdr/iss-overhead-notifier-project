d6ea27d8f9e91b0ce658b93d7ed023ea406c597e
TheREALAdr <anotidamurambadoro455@gmail.com>
2023-06-20 15:28:18 -0500

    ISS checking function made (should work but haven't tested). Try running it every 60 seconds.

--- main.py
+++ main.py
@@ -1,8 +1,25 @@
+# ---------------------- IMPORTS ---------------------- #
+
+import smtplib
 import requests
 from datetime import datetime
 
+# ---------------------- CONSTANTS -------------------- #
+
+# PERSONAL LOCATION CONSTANTS
 MY_LAT = 51.507351 # Your latitude
 MY_LONG = -0.127758 # Your longitude
+
+# EMAIL CONSTANTS
+sending_gmail_address = "obviouslyafakeemailbroski@gmail.com"
+sending_gmail_app_password = "vqtlvqzpnjhufmto"
+yahoo_testing_acc = "testing_468@yahoo.com"
+
+# DATETIME CONSTANTS
+time_now = datetime.now()
+hour_now = time_now.hour
+
+# ---------- REQUEST/CONSTANTS FOR ISS POSITION API ------------ #
 
 response = requests.get(url="http://api.open-notify.org/iss-now.json")
 response.raise_for_status()
@@ -11,14 +28,13 @@ data = response.json()
 iss_latitude = float(data["iss_position"]["latitude"])
 iss_longitude = float(data["iss_position"]["longitude"])
 
-#Your position is within +5 or -5 degrees of the ISS position.
-
-
 parameters = {
     "lat": MY_LAT,
     "lng": MY_LONG,
     "formatted": 0,
 }
+
+# -------------- REQUEST/CONSTANTS FOR SUNRISE-SUNSET API --------------- #
 
 response = requests.get("https://api.sunrise-sunset.org/json", params=parameters)
 response.raise_for_status()
@@ -26,12 +42,30 @@ data = response.json()
 sunrise = int(data["results"]["sunrise"].split("T")[1].split(":")[0])
 sunset = int(data["results"]["sunset"].split("T")[1].split(":")[0])
 
-time_now = datetime.now()
 
-#If the ISS is close to my current position
-# and it is currently dark
-# Then send me an email to tell me to look up.
+# ----------------------- CHECK ISS POSITION/TIME & SEND EMAIL ---------------------- #
+
+
+# TODO: Check if your location is within +5 or -5 degrees of the ISS position.
+
+def check_for_iss():
+    latitude_distance_difference = abs(iss_latitude - MY_LAT)
+    longitude_distance_difference = abs(iss_longitude - MY_LONG)
+    if latitude_distance_difference <= 5 and longitude_distance_difference <= 5:
+        if sunset < hour_now < sunrise:
+            with smtplib.SMTP("smtp.gmail.com") as connection:
+                connection.starttls()
+                connection.login(user=sending_gmail_address, password=sending_gmail_app_password)
+                connection.sendmail(from_addr=sending_gmail_address,
+                                    to_addrs=yahoo_testing_acc,
+                                    msg=f"Subject:Look up!\n\nThe ISS is overhead in your location."
+                                    )
+
+
+# -------------------- SENDING THE EMAIL -------------------- #
+
 # BONUS: run the code every 60 seconds.
+# While loop or detection method to make the count stop once the ISS disappears?
 
 
 
